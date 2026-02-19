6e2f40cdfdeaac8b82730b57e0cda6207369fdae
adrienMGN <adrien@mbp-de-adrien.home>
2026-02-19 18:59:12 +0100

    feat: secrets db

--- crazy-charly-web/entrypoint.sh
+++ crazy-charly-web/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -e
 
+# Load secrets if they exist
+if [ -f /run/secrets/db_password ]; then
+  DB_PASSWORD=$(cat /run/secrets/db_password | tr -d '\n\r')
+  DB_USER=$(cat /run/secrets/db_user | tr -d '\n\r')
+  DB_NAME=$(cat /run/secrets/db_name | tr -d '\n\r')
+  
+  export DATABASE_URL="postgresql://${DB_USER}:${DB_PASSWORD}@db:5432/${DB_NAME}?schema=public"
+  echo "Using secrets for database connection."
+fi
+
 until nc -z db 5432; do
   echo "Waiting for database..."
   sleep 2
@@ -9,13 +19,13 @@ done
 echo "--- Database is ready! ---"
 
 echo "--- Generating Prisma Client ---"
-npx prisma generate
+npx prisma@5 generate
 
 echo "--- Running Prisma DB Push ---"
-npx prisma db push --accept-data-loss
+npx prisma@5 db push --accept-data-loss
 
 echo "--- Running Prisma DB Seed ---"
-npx prisma db seed
+npx prisma@5 db seed
 
 echo "--- Starting Application ---"
 exec "$@"
