cf363048662ab3189a51f93ec531d00790f1d756
Cyprian <cyprian.chailan@icloud.com>
2026-02-19 18:44:23 +0100

    modif abonne

--- optimisation/src/models/types.ts
+++ optimisation/src/models/types.ts
@@ -1,5 +1,6 @@
 export type Categorie = 'SOC' | 'FIG' | 'CON' | 'EXT' | 'EVL' | 'LIV';
 
 export type TrancheAge = 'BB' | 'PE' | 'EN' | 'AD';
+export const AGE_TRANCHES: TrancheAge[] = ['BB', 'PE', 'EN', 'AD'];
 
 export type Etat = 'N' | 'TB' | 'B';
