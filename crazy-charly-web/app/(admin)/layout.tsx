5a8b9cecdfac4bfdb8333dee2b7b9e90c2a43d2d
Julien Defolie <julien.defolie@gmail.com>
2026-02-19 21:54:07 +0100

    fix: jwt admin/subscriber

--- crazy-charly-web/app/(admin)/layout.tsx
+++ crazy-charly-web/app/(admin)/layout.tsx
@@ -1,11 +1,24 @@
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
 import Header from "@/components/Header";
 import BottomTabBar from "@/components/BottomTabBar";
 
-export default function AdminLayout({
+export default async function AdminLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    // => jamais de problème de JWT périmé
+    const session = await auth();
+
+    if (!session) {
+        redirect("/connexion");
+    }
+
+    if (session.user?.role !== "admin") {
+        redirect("/acces-refuse");
+    }
+
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
